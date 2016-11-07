fc5ae54d27c6b6269a61055606475f0bcfbad10c
Hunter Hays <hhays@yahoo-inc.com>
2016-11-07 11:01:54 -0800

    second commit todo app - everything is working per the required stories

--- app/src/main/java/com/hphays/todoapp/EditItemActivity.java
+++ app/src/main/java/com/hphays/todoapp/EditItemActivity.java
@@ -1,26 +1,49 @@
 package com.hphays.todoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import static com.hphays.todoapp.R.id.etEditText;
 
 public class EditItemActivity extends AppCompatActivity {
 
+    EditText text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
+        setTitle("Edit Activity");
+        text = (EditText) findViewById(R.id.editText);
+        text.setText(getIntent().getStringExtra("text"));
+        text.setSelection(text.getText().length());
     }
 
     // ActivityTwo.java
     public void onSubmit(View v) {
         // closes the activity and returns to first screen
+        launchMainView(text.getText().toString(), getIntent().getIntExtra("position", 0));
         this.finish();
-
     }
 
+
+    public void launchMainView(String text, int position) {
+        // first parameter is the context, second is the class of the activity to launch
+        Intent i = new Intent();
+        i.putExtra("text", text);
+        i.putExtra("position", position);
+
+
+
+        setResult(RESULT_OK, i); // brings up the main activity
+    }
     public void onEditItem(View view) {
         onSubmit(view);
     }
--- app/src/main/java/com/hphays/todoapp/MainActivity.java
+++ app/src/main/java/com/hphays/todoapp/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -24,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     EditText etEditText;
 
+    private final int REQUEST_CODE = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateArrayItems();
-        lvItems =(ListView) findViewById(R.id.lvItems);
+        lvItems = (ListView) findViewById(R.id.lvItems);
         lvItems.setAdapter(itemsAdaptor);
         etEditText = (EditText) findViewById(R.id.etEditText);
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -45,23 +49,24 @@ public class MainActivity extends AppCompatActivity {
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                launchComposeView(position, items);
+                launchComposeView(items.get(position), position);
             }
         });
     }
+
     public void populateArrayItems() {
         readItems();
-        itemsAdaptor = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
+        itemsAdaptor = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
 
     }
 
     // ActivityOne.java
-    public void launchComposeView() {
+    public void launchComposeView(String text, int position) {
         // first parameter is the context, second is the class of the activity to launch
         Intent i = new Intent(MainActivity.this, EditItemActivity.class);
-        i.putExtra("text", "foobar");
-        i.putExtra("in_reply_to", "george");
-        startActivity(i); // brings up the second activity
+        i.putExtra("text", text);
+        i.putExtra("position", position);
+        startActivityForResult(i, REQUEST_CODE); // brings up the second activity
     }
 
     private void readItems() {
@@ -88,5 +93,22 @@ public class MainActivity extends AppCompatActivity {
         itemsAdaptor.add(etEditText.getText().toString());
         etEditText.setText("");
         writeItems();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String text = data.getExtras().getString("text");
+            int position = data.getExtras().getInt("position", 0);
+
+            items.set(position, text);
+
+            writeItems();
+
+            itemsAdaptor.notifyDataSetChanged();
+
+        }
     }
 }
